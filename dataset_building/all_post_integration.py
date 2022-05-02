5023f6c2b24549d7c5553706062769ac821a5406
Shanto Roy <roy@Shantos-MacBook-Pro.local>
2022-05-02 10:35:38 -0500

    updated codebase with analysis and fixed scrapping issues

--- dataset_building/all_post_integration.py
+++ dataset_building/all_post_integration.py
@@ -1,47 +1,51 @@
-"""
-This program integrates all the relevant posts from
-JSON files for each keyword. We check relevancy by searching
-the keyword either in title or in tag. The program also
-prints the number of relevant items from each individual file.
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   all_post_integration.py
+@Time    :   2022/05/01 20:47:26
+@Author  :   Shanto Roy 
+@Version :   1.0
+@Contact :   sroy10@uh.edu
+@Desc    :   This program integrates all the relevant posts from
+             JSON files for each keyword. We check relevancy by searching
+             the keyword either in title or in tag. The program also
+             prints the number of relevant items from each individual file.
+'''
 
-Author : Shanto Roy
-Year : 2019
-"""
 
 import json
+import os
 final_count = 0
 final_json_data = []
 
-file_name_list = ["smart-contract.json", "smart-contracts.json", "metamask.json",
-                      "solidity.json", "vyper.json",
-                      "truffle.json", "web3.json", "erc20.json", "security.json"]
-tag_list = ["smart contracts", "smart contract", "metamask", "solidity", "vyper",
-                "smart contract", "web3", "erc20", "security"]
+path = os.path.join(os.getcwd(), "data/individual_tag_data")
+# print(path)
+
+file_name_list = os.listdir(path)
+# remove unnecessary files
+file_name_list = [i for i in file_name_list if i.endswith('.json')]
+tag_list = [i[:len(i)-5] for i in file_name_list]
 
 for file_name, tag in zip(file_name_list, tag_list):
 
     count = 0
-    json_data = json.load(open("post_data/"+file_name))
+    json_data = json.load(open(os.path.join(path,file_name)))
 
     # Check if posts are relevant by checking tags in title or tag
     for key in json_data:
-        tags = key['tags']
-        titles = key['title']
-
-        if tag in key['title'].lower():
+        try:
             final_json_data.append(key)
             count += 1
-        else:
-            for item in tags:
-                if tag in item.lower():
-                    final_json_data.append(key)
-                    count += 1
-                    final_count += 1
+            final_count += 1
+        except Exception as e:
+            print(str(e))
+
     print("Number of total post for ", tag, "is =", count)
 
 
 # Output the updated file with pretty JSON
-open("integrated_all_post_data_final.json", "w").write(
+target_filename = "data/integrated_all_post_data_final.json"
+open(os.path.join(os.getcwd(),target_filename), "w").write(
         json.dumps(final_json_data, sort_keys=True, indent=4, separators=(',', ': '))
     )
 
--- dataset_building/remove_duplicate_items_json.py
+++ dataset_building/remove_duplicate_items_json.py
@@ -1,6 +1,23 @@
-import json
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   remove_duplicate_items_json.py
+@Time    :   2022/05/01 21:15:00
+@Author  :   Shanto Roy 
+@Version :   1.0
+@Contact :   sroy10@uh.edu
+@Desc    :   This script removes the duplicate posts
+'''
 
-with open("integrated_all_post_data_final.json", "r") as f:
+
+import json
+import os
+
+path = os.path.join(os.getcwd(), "data")
+
+input_filename = "integrated_all_post_data_final.json"
+
+with open(os.path.join(path,input_filename), "r") as f:
     data = json.load(f)
 unique_post = {each['title']: each for each in data}
 listed_dictionary_items = list(unique_post.values())
@@ -13,6 +30,8 @@ listed_dictionary_items = list(unique_post.values())
 #             listed_dictionary_items.remove(key)
 #     print("Number of Final posts:", len(listed_dictionary_items))
 
-with open("related_data_rm_duplicacy_final_conf.json", "w") as f:
+output_filename = "related_data_rm_duplicacy_final.json"
+
+with open(os.path.join(path,output_filename), "w") as f:
     json.dump(listed_dictionary_items, f)
 print("# of post after removing duplicacy = ", len(listed_dictionary_items))
